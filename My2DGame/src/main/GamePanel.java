45495aae7ac2652a3c057edbbf169a366455326f
joshvanedwardabanico <150789446+joshvanedwardabanico@users.noreply.github.com>
2024-11-02 15:47:09 +0100

    Added tile package,tile manager and images

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
 	
@@ -24,15 +25,12 @@ public class GamePanel extends JPanel implements Runnable {
 	//FPS
 	int FPS = 60;
 	
+	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	Player player = new Player(this, keyH);
 	
-	//Set player's default position
-	int playerX = 100;
-	int playerY = 100;
-	int playerSpeed = 4;
-	
+
 	public GamePanel() {
 		
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -89,6 +87,8 @@ public class GamePanel extends JPanel implements Runnable {
 		super.paintComponent(g);
 		
 		Graphics2D g2 = (Graphics2D)g; 
+		
+		tileM.draw(g2);
 		
 		player.draw(g2);
 
